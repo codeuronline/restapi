b9dcb7f80d91cf90b4e1b15432c138e3a1faf114
codeuronline <j.kasperski@codeur.online>
2022-04-25 11:59:54 +0200

    update database scheme

--- micromarket.sql
+++ micromarket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : lun. 25 avr. 2022 à 08:57
+-- Généré le : lun. 25 avr. 2022 à 09:58
 -- Version du serveur : 5.7.33
 -- Version de PHP : 7.4.27
 
@@ -24,10 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `produits`
+-- Structure de la table `assets`
 --
 
-CREATE TABLE `produits` (
+CREATE TABLE `assets` (
+  `id` int(11) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `file_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `nom`) VALUES
+(1, 'Boulangerie/Pâtisserie'),
+(2, 'Epicerie Salée'),
+(3, 'Epicerie sucrée'),
+(4, 'Boissons'),
+(5, 'Fromagerie'),
+(6, 'Poissonnerie'),
+(7, 'Boucherie'),
+(8, 'Libre-service'),
+(9, 'Vente à l\'étalage'),
+(10, 'Tête de gondole');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products`
+--
+
+CREATE TABLE `products` (
   `id_product` int(11) NOT NULL,
   `code` varchar(5) NOT NULL,
   `description` varchar(255) NOT NULL,
@@ -36,29 +75,166 @@ CREATE TABLE `produits` (
   `statut_id` int(11) NOT NULL,
   `supplier_id` int(11) NOT NULL,
   `purchase_date` datetime NOT NULL,
-  `expiration_date` datetime NOT NULL
+  `expiration_date` datetime NOT NULL,
+  `primary_visual` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product_assets`
+--
+
+CREATE TABLE `product_assets` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `asset_id` int(11) NOT NULL,
+  `flag_primary` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product_category`
+--
+
+CREATE TABLE `product_category` (
+  `id` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL,
+  `id_category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product_status`
+--
+
+CREATE TABLE `product_status` (
+  `id` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `id_statut` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `statut`
+--
+
+CREATE TABLE `statut` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `statut`
+--
+
+INSERT INTO `statut` (`id`, `name`) VALUES
+(1, 'En cours d\'approvisionnement'),
+(2, 'En Stock'),
+(3, 'Epuisé'),
+(4, 'Retiré des rayons');
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `produits`
+-- Index pour la table `assets`
 --
-ALTER TABLE `produits`
+ALTER TABLE `assets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
   ADD PRIMARY KEY (`id_product`);
-ALTER TABLE `produits` ADD FULLTEXT KEY `description` (`description`);
+ALTER TABLE `products` ADD FULLTEXT KEY `description` (`description`);
+
+--
+-- Index pour la table `product_assets`
+--
+ALTER TABLE `product_assets`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`,`product_id`),
+  ADD UNIQUE KEY `asset` (`asset_id`);
+
+--
+-- Index pour la table `product_category`
+--
+ALTER TABLE `product_category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `produits` (`id_produit`),
+  ADD UNIQUE KEY `categorys` (`id_category`);
+
+--
+-- Index pour la table `product_status`
+--
+ALTER TABLE `product_status`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `statut` (`id_statut`),
+  ADD UNIQUE KEY `product` (`id_product`);
+
+--
+-- Index pour la table `statut`
+--
+ALTER TABLE `statut`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `produits`
+-- AUTO_INCREMENT pour la table `assets`
 --
-ALTER TABLE `produits`
+ALTER TABLE `assets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `products`
+--
+ALTER TABLE `products`
   MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `product_assets`
+--
+ALTER TABLE `product_assets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `product_category`
+--
+ALTER TABLE `product_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `product_status`
+--
+ALTER TABLE `product_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `statut`
+--
+ALTER TABLE `statut`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
