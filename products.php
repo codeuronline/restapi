d35bb6f498a5c8ec2f97ed3ff768b87cd5ca0d88
codeuronline <j.kasperski@codeur.online>
2022-04-28 08:49:16 +0200

    update

--- products.php
+++ products.php
@@ -4,22 +4,28 @@ define("URL", str_replace("products.php", "", (isset($_SERVER['HTTPS']) ? "https
 require_once "controllers/ProductsController.php";
 $productController = new ProductsController;
 //require "views/accueil.view.php";
+switch ($_SERVER['REQUEST_METHOD']){
+    case 'GET':
+        if (!(empty($_GET))){
+            $url = explode("/", filter_var($_GET['id']), FILTER_SANITIZE_URL);
+            if (empty($url[0])) {
+                $productController->afficherProducts();
+            } else if ($url[0]) {
+                //ici j'affiche 1seul produit
+                $productController->afficherProduct($url[0]);
+            }
 
-if (!(empty($_GET))){
-    $url = explode("/", filter_var($_GET['id']), FILTER_SANITIZE_URL);
-   if (empty($url[0])) {
+        }else{
             $productController->afficherProducts();
-    } else if ($url[0]) {
-            //ici j'affiche 1seul produit
-            $productController->afficherProduct($url[0]);
-        
         }
-
-}else{
-    
-       $productController->afficherProducts();
-
-    }
-
-        
-?>
+        break;
+    case 'POST':
+        $POST = array(); //tableau qui va contenir les données reçues
+        parse_str(file_get_contents('php://input'), $POST);
+        break;
+    case "PUT": 
+        $_PUT = array(); //tableau qui va contenir les données reçues
+        parse_str(file_get_contents('php://input'), $_PUT);
+        break;
+    case "DELETE": break;
+    }   
