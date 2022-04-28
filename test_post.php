0634c148ee9a0aa7b232d76e0db27d533b28e6d4
codeuronline <j.kasperski@codeur.online>
2022-04-28 11:52:37 +0200

    up3

--- test_post.php
+++ test_post.php
@@ -1,7 +1,6 @@
 <?php
 $url = "http://localhost/restapi/products.php"; // modifier le produit 1
 $data = array(
-<<<<<<< HEAD
     'code' => 'MOD',
     'description' => 'Moutarde de Dijon',
     'price' => 225,
@@ -10,16 +9,6 @@ $data = array(
     'supplier' => 3,
     'purchase' => '2022-04_01 10:40:00',
     'expire' => '2023-04-01 10:40:00'
-=======
-'code' => 'MOD',
-'description' => 'Moutarde de Dijon',
-'price' => 225,
-'category' => 1,
-'statut' => 2,
-'supplier' => 3,
-'purchase' => '2022-04_01 10:40:00',
-'expire' => '2023-04-01 10:40:00'
->>>>>>> 3f7302caee2ecaa42f1de2651a78f9019f8640c8
 );
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -28,12 +17,5 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 $response = curl_exec($ch);
 var_dump($response);
 if (!$response) {
-<<<<<<< HEAD
     return false;
 }
-=======
-return false;
-}
-
-?>
->>>>>>> 3f7302caee2ecaa42f1de2651a78f9019f8640c8
