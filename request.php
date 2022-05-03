769fa9bc6164ec3c7955921e92e9d8df4ba83e25
PCGameJK <j.kasperski@codeur.online>
2022-05-03 07:37:44 +0200

    update method post

--- request.php
+++ request.php
@@ -3,9 +3,16 @@ require_once "controllers/ProductsController.php";
 $productController = new ProductsController;
 // changement de status
 
-extract($_GET);
-$productController->modificationProductRequest($id_product,$statut_id);
-$answerPHP= true;
+switch($_SERVER['REQUEST_METHOD']){
+    case "GET": extract($_GET);
+                $productController->modificationProductRequest($id_product,$statut_id);
+                 $answerPHP= true;
+                 break;
+    case "POST": extract($POST);
+                $productController->modificationProductRequest($id_product,$statut_id);
+                $answerPHP=true;
+                break;
+}
 // requete d'update 
 echo json_encode($answerPHP);
 
