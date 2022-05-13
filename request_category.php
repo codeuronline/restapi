c0a7b6bfc43c759b4bd835e1cafbd58536af94e6
PCGameJK <j.kasperski@codeur.online>
2022-05-13 17:17:29 +0200

    update fullajax

--- request_category.php
+++ request_category.php
@@ -1,12 +1,17 @@
 <?php
 //parse_str(file_get_contents('php://input'), $url);
+$urlRequest = explode("/", filter_var(@$_GET['id']), FILTER_SANITIZE_URL);
 error_log("traitement de la requete categorie avec le parametre ".$url[1]);
 error_log(print_r($url[1]),1);
 error_log(print_r($url[0]),1);
 //error_log(print_r($urlrequest[2]),1);
 
 //error_log($url);
-$productController->loadProductRequest($url[1]);
+if (isset($urlRequest[2])){
+    $productController->loadProductRequest($url[2]);
+}else{
+    $productController->loadProductRequest($url[1]);
+}
  $answerPHP= true;
      
 // requete d'update 
