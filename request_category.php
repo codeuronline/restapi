13b74238eb52a3006e74a7b87f0b4277b7aebb20
codeuronline <j.kasperski@codeur.online>
2022-05-16 08:47:29 +0200

    update ajax

--- request_category.php
+++ request_category.php
@@ -1,18 +1,19 @@
 <?php
 //parse_str(file_get_contents('php://input'), $url);
-$urlRequest = explode("/", filter_var(@$_GET['id']), FILTER_SANITIZE_URL);
-error_log("traitement de la requete categorie avec le parametre ".$url[1]);
-error_log(print_r($url[1]),1);
-error_log(print_r($url[0]),1);
-//error_log(print_r($urlrequest[2]),1);
-
-//error_log($url);
-if (isset($urlRequest[2])){
-    
+error_log("---------------------------------");
+error_log("fichier request category");
+$urlrequest=explode("/", filter_var($_GET['id']), FILTER_SANITIZE_URL);
+error_log("*******[".isset($urlrequest)."]*******");
+if (isset($url[1])=='request') {
     $productController->loadProductRequest($url[2]);
-}else{
-    $productController->loadProductRequest($url[1]);
+} else{
+    $productController->loadProductRequest($urlrequest[1]);
 }
+        
+    
+
+
+
  $answerPHP= true;
      
 // requete d'update 
